c0b3453db0a32dc7f5c0e688c0738b8eaa36899d
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-01 15:53:55 +0300

    Chapter 8, Exercise 6

--- main.c
+++ main.c
@@ -8,50 +8,40 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on March 29, 2016, 1:52 PM
+ * Created on March 30, 2016, 2:21 PM
  */
 
-/* 
- * Chapter 8, Exercise 5
- * I can't understand the fuckin' maths in this exercise 
- */
+/* Modify Program 8.8 so that the squareRoot function accepts a double
+ precision argument and returns the result as a double precision value. Be
+ Certain to change the value of the variable epsilon to reflect the fact that
+ double precision values are now being used.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Chapter 8, Exercise 6
+ */
 // Function to calculate the absolute value of a number
-
-float absoluteValue (float x) {
-    if ( x < 0 )
+double absoluteValue(double x) {
+    if (x < 0)
         x = -x;
-    return (x);
+    return x;    
 }
 
 // Function to compute the square root of a number
-
-float squareRoot (float x) {
-    const float epsilon = .00001;
-    float       guess = 1.0;
+double squareRoot(double x) {
+    const double epsilon = .00001;
+    double guess = 1.0;
     
-    /* Here is a solution from http://www.linuxquestions.org/questions/programming-9/simple-c-program-but-stuck-please-help-732707/:
-     * while ( absoluteValue( 1 - (guess*guess)/x )  >=  epsilon )
-     * Is it correct???
-    */
-    while ( absoluteValue (guess * guess -x) >= epsilon ) {
-        printf("guess = %f\n", guess);
+    while ( absoluteValue(guess * guess -x) >= epsilon )
         guess = ( x / guess + guess ) / 2.0;
-        printf ("guess after assignment = %f\n", guess);
-    }
     return guess;
 }
-
-/*
- * 
- */
 int main(int argc, char** argv) {
-    printf ("squareRoot (2.0) = %f\n", squareRoot (2.0));
-    printf ("squareRoot (144.0) = %f\n", squareRoot (144.0));
-    printf ("squareRoot (17.5) = %f\n", squareRoot (17.5));
+    printf("squareRoot (2.0) = %f\n", squareRoot(2.0));
+    printf("squareRoot (144.0) = %f\n", squareRoot(144.0));
+    printf("squareRoot (17.5) = %f\n", squareRoot(17.5));
 
     return (EXIT_SUCCESS);
 }
