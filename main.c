b2f383dea469a1d09993408dadd65b55e2487cf3
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-01 15:46:42 +0300

    Chapter 8, Exercise 5

--- main.c
+++ main.c
@@ -8,45 +8,51 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 1, 2016, 3:32 PM
+ * Created on March 29, 2016, 1:52 PM
  */
 
-/* Modify Program 8.8 so that the value of guess is printed each
-time through the while loop. Notice how quickly the value of guess
-converges to the square root. What conclusions can you reach about 
-the number of iterations through the loop, the number whose square
-root is being calculated, and the value of the initial guess? */
+/* 
+ * Chapter 8, Exercise 5
+ * I can't understand the fuckin' maths in this exercise 
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * Chapter 8, Exercise 4
- */
-
 // Function to calculate the absolute value of a number
+
 float absoluteValue (float x) {
     if ( x < 0 )
         x = -x;
-        return (x);
+    return (x);
 }
 
 // Function to compute the square root of a number
+
 float squareRoot (float x) {
     const float epsilon = .00001;
     float       guess = 1.0;
     
-    while ( absoluteValue ( guess * guess - x ) >= epsilon ) {
+    /* Here is a solution from http://www.linuxquestions.org/questions/programming-9/simple-c-program-but-stuck-please-help-732707/:
+     * while ( absoluteValue( 1 - (guess*guess)/x )  >=  epsilon )
+     * Is it correct???
+    */
+    while ( absoluteValue (guess * guess -x) >= epsilon ) {
         printf("guess = %f\n", guess);
-        guess = ( x / guess + guess ) / 2;
+        guess = ( x / guess + guess ) / 2.0;
+        printf ("guess after assignment = %f\n", guess);
     }
-        
     return guess;
 }
 
+/*
+ * 
+ */
 int main(int argc, char** argv) {
-    printf("squareRoot (2.0) = %f\n", squareRoot (2.0));
-    printf("squareRoot (144.0) = %f\n", squareRoot (144.0));
-    printf("squareRoot (17.5) = %f\n", squareRoot (17.5));
+    printf ("squareRoot (2.0) = %f\n", squareRoot (2.0));
+    printf ("squareRoot (144.0) = %f\n", squareRoot (144.0));
+    printf ("squareRoot (17.5) = %f\n", squareRoot (17.5));
+
     return (EXIT_SUCCESS);
 }
+
