d3c9750e3a8171673895bd54372d5dfd42930b14
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-01 16:24:26 +0300

    Chapter 8, Exercise 2a

--- main.c
+++ main.c
@@ -8,25 +8,19 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 1, 2016, 1:50 PM
+ * Created on April 1, 2016, 3:04 PM
  */
 
 /* Modify Program 8.4 so the value of triangularNumber is returned by 
  * the function. Then go back to Program 5.5 and change that program
  * so that it calls the new version of the calculateTriangularNumber
  * function. */
- 
- /* I have removed printf from inside the function, and added tree new
-  * variables in which to store the result returned from the 
-  * calculateTriangularNumber function. I also defined the function as
-  * an int (insted of void as it was. The tree new variables correspond
-  * to the parameters given to the function. */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
- * Chapter 8, Exercise 2
+ * 
  */
 
 int calculateTriangularNumber (int n) {
@@ -39,13 +33,22 @@ int calculateTriangularNumber (int n) {
 }
 
 int main(int argc, char** argv) {
-    int ten, twenty, fifty;
+    int n, number, triangularNumber, counter;
     
-    ten = calculateTriangularNumber (10);
-    twenty = calculateTriangularNumber (20);
-    fifty = calculateTriangularNumber (50);
+    int calculateTriangularNumber (int n);
     
-    printf ("ten = %i\ntwenty = %i\nfifty = %i\n", ten, twenty, fifty);
+    for ( counter = 1; counter <= 5; ++counter ) {
+        printf("What triangular number do you want? ");
+        scanf("%i", &number);
+        triangularNumber = calculateTriangularNumber (number);
+        
+        /*triangularNumber = 0;
+        
+        for ( n = 1; n <= number; ++n )
+            triangularNumber += n;*/
+            
+        printf("Triangular number %i is %i\n\n", number, triangularNumber);
+    }
 
     return (EXIT_SUCCESS);
 }
