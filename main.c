9997b1c99203d793c07668ea94b1bdbc7931bdbe
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-01 15:37:40 +0300

    Chapter 8, Exercise 4

--- main.c
+++ main.c
@@ -8,18 +8,20 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 1, 2016, 3:27 PM
+ * Created on April 1, 2016, 3:32 PM
  */
 
-/* Modify Program 8.8 so the value of epsilon is passed as an argument to
- * the function. Try experimenting with different values of epsilon to
- * see the effect that it has on the value of square root */
+/* Modify Program 8.8 so that the value of guess is printed each
+time through the while loop. Notice how quickly the value of guess
+converges to the square root. What conclusions can you reach about 
+the number of iterations through the loop, the number whose square
+root is being calculated, and the value of the initial guess? */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
- * 
+ * Chapter 8, Exercise 4
  */
 
 // Function to calculate the absolute value of a number
@@ -30,21 +32,21 @@ float absoluteValue (float x) {
 }
 
 // Function to compute the square root of a number
-float squareRoot (float x, const float epsilon) {
-    //const float epsilon = .00001;
+float squareRoot (float x) {
+    const float epsilon = .00001;
     float       guess = 1.0;
     
-    while ( absoluteValue ( guess * guess - x) >= epsilon )
+    while ( absoluteValue ( guess * guess - x ) >= epsilon ) {
+        printf("guess = %f\n", guess);
         guess = ( x / guess + guess ) / 2;
+    }
         
     return guess;
 }
 
 int main(int argc, char** argv) {
-    printf("squareRoot (2.0) = %f\n", squareRoot (2.0, .01000));
-    printf("squareRoot (144.0) = %f\n", squareRoot (144.0, 00001));
-    printf("squareRoot (17.5) = %f\n", squareRoot (17.5, 00001));
-
+    printf("squareRoot (2.0) = %f\n", squareRoot (2.0));
+    printf("squareRoot (144.0) = %f\n", squareRoot (144.0));
+    printf("squareRoot (17.5) = %f\n", squareRoot (17.5));
     return (EXIT_SUCCESS);
 }
-
