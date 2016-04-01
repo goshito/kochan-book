466537134643e1b3d27040edaed5a5a347226a52
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-01 15:55:19 +0300

    Chapter 8, Exercise 7

--- main.c
+++ main.c
@@ -8,40 +8,35 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on March 30, 2016, 2:21 PM
+ * Created on March 30, 2016, 9:59 PM
  */
 
-/* Modify Program 8.8 so that the squareRoot function accepts a double
- precision argument and returns the result as a double precision value. Be
- Certain to change the value of the variable epsilon to reflect the fact that
- double precision values are now being used.*/
+/*
+ * Write a function that raises an integer to a positibe integer power. Call
+ * the function x_to_the_n taking two integer arguments x and n. Have the func-
+ * return a long int, which represents the results of calculating xn.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
- * Chapter 8, Exercise 6
+ * Chapter 8, Exercise 7
  */
-// Function to calculate the absolute value of a number
-double absoluteValue(double x) {
-    if (x < 0)
-        x = -x;
-    return x;    
+
+long int x_to_the_n(int x, int n){
+    long int result;
+    result = x;
+    int i = 1;
+    while (i < n) {
+        result *= x;
+        ++i;
+    }
+    return result;
 }
 
-// Function to compute the square root of a number
-double squareRoot(double x) {
-    const double epsilon = .00001;
-    double guess = 1.0;
-    
-    while ( absoluteValue(guess * guess -x) >= epsilon )
-        guess = ( x / guess + guess ) / 2.0;
-    return guess;
-}
 int main(int argc, char** argv) {
-    printf("squareRoot (2.0) = %f\n", squareRoot(2.0));
-    printf("squareRoot (144.0) = %f\n", squareRoot(144.0));
-    printf("squareRoot (17.5) = %f\n", squareRoot(17.5));
+    printf("%li", x_to_the_n(2, 4));
 
     return (EXIT_SUCCESS);
 }
