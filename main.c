951336abc846adb283674a88661ea23cccca6d80
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-10 16:59:10 +0300

    Chapter 9, Exercise 5

--- main.c
+++ main.c
@@ -8,90 +8,135 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 10, 2016, 1:04 PM
+ * Created on April 10, 2016, 2:07 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /*
- * Chapter 9, Exercise 4
+ * Chapter 9, Exercise 5
  */
 
-/* N = 1461 x f(year, month) / 4 + 153 x g(month) / 5 + day
- * If you take the value of N as computed in exercise 2, subtract 621,049 from
- * it, and then take the result modulo 7, you get a number from 0 to 6 that
- * represents the day of the week (Sunday through Saturday, respectively) on
- * whihc the particular day falls. For example, the value of N computed for
- * August 8, 2004, is 732,239 as derived previously. 732,239 - 621,049 gives
- * 11,190, and 111,190 % 7 gives 2, indicating that this date falls on Tuesday.
- * 
- * Use the functions developed in the previous exercise to develop a program
- * that displays the day of the week on which a particular date falls. Make
- * certain that the program displays the day of the week in English (such as
- * "Monday"). */
+/* Write a function called clockKeeper that takes as its argument a dateAndTime
+ * structure as defined in this chapter. The function should call the timeUpdate
+ * function, and if the time reaches midnight, the function should call the
+ * dateUpdate function to switch over to the next day. Have the function return
+ * the updated dateAndTime structure. */
 
 struct date {
-        int month;
-        int day;
-        int year;
-    };
+    int month;
+    int day;
+    int year;
+};
 
-// Function to calculate f(year, month) and return the value of year 
+struct time {
+    int hour;
+    int minutes;
+    int seconds;
+};
 
-int f(int year, int month) {
-    if (month <= 2)
-        return year - 1;
-    else
-        return year;
+struct dateAndTime {
+    struct date sdate;
+    struct time stime;
+};
+
+// Function to update time by one second
+
+struct time timeUpdate(struct time now) {
+    ++now.seconds;
+    
+    if (now.seconds == 60) {        // next minute
+        now.seconds = 0;
+        ++now.minutes;
+        
+        if (now.minutes == 60) {    // next hour
+            now.minutes = 0;
+            ++now.hour;
+            
+            if (now.hour == 24)     // midnight
+                now.hour = 0;
+        }
+    }
+    
+    return now;
 }
 
-// Function to calculate g(month) 
+// Function to find the number of days in a month
 
-int g(int month) {
-    if (month <= 2)
-        return month + 13;
+int numberOfDays(struct date d) {
+    int days;
+    bool isLeapYear(struct date d);
+    const int daysPerMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    
+    if (isLeapYear && d.month == 2)
+        days = 29;
     else
-        return month + 1;
+        days = daysPerMonth[d.month - 1];
+    
+    return days;
 }
 
-// Function to calculate N
+// Function to determine if it's a leap year
 
-int calcDays(struct date date) {
-    int n;
+bool isLeapYear(struct date d) {
+    bool leapYearFlag;
     
-    n = 1461 * f(date.year, date.month) / 4 + 153 * g(date.month) / 5 + date.day;
+    if ((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0)
+        leapYearFlag = true;    // It's a leap year
+    else
+        leapYearFlag = false;   // Not a leap year
     
-    return n;
+    return leapYearFlag;
+}
+
+// Function to calculate tomorrow's date
+
+struct date dateUpdate(struct date today) {
+    struct date tomorrow;
+    int numberOfDays(struct date d);
+    
+    if (today.day != numberOfDays(today)) {
+        tomorrow.day = today.day + 1;
+        tomorrow.month = today.month;
+        tomorrow.year = today.year;
+    } else if (today.month == 12) {     // end of year
+        tomorrow.day = 1;
+        tomorrow.month = 1;
+        tomorrow.year = today.year + 1;
+    } else {                            // end of month
+        tomorrow.day = 1;
+        tomorrow.month = today.month + 1;
+        tomorrow.year = today.year;
+    }
+    
+    return tomorrow;
+}
+
+// And here's the fuckin' clockKeeper function
+
+struct dateAndTime clockKeeper(struct dateAndTime dat) {
+    dat.stime = timeUpdate(dat.stime);
+    if (dat.stime.hour == 0)
+        dat.sdate = dateUpdate(dat.sdate);
+    
+    return dat;
 }
 
 int main(int argc, char** argv) {
-    int n, week_day;
-    struct date date;
+    struct dateAndTime test;
     
-    printf("Enter a date (mm/dd/yyyy): ");
-    scanf("%i/%i/%i", &date.month, &date.day, &date.year);
+    printf("Enter date (mm/dd/yyyy): ");
+    scanf("%i/%i/%i", &test.sdate.month, &test.sdate.day, &test.sdate.year);
+    printf("\nEnter time (hh:mm:ss): ");
+    scanf("%i:%i:%i", &test.stime.hour, &test.stime.minutes, &test.stime.seconds);
     
-    n = calcDays(date);
-    week_day = (n - 621049) % 7;
+    test = clockKeeper(test);
     
-    if (week_day == 0)
-        printf("Sunday");
-    else if (week_day == 1)
-        printf("Monday");
-    else if (week_day == 2)
-        printf("Tuesday");
-    else if (week_day == 3)
-        printf("Wednesday");
-    else if (week_day == 4)
-        printf("Thrusday");
-    else if (week_day == 5)
-        printf("Friday");
-    else if (week_day == 6)
-        printf("Saturday");
-    else
-        printf("!!! Critical Error !!!\n MUHAHAHAHAHAHA");
-
+    printf("\nDate:\n%i/%i/%i", test.sdate.month, test.sdate.day, test.sdate.year);
+    printf("\nTime:\n%i:%i:%i", test.stime.hour, test.stime.minutes, test.stime.seconds);
+    
     return (EXIT_SUCCESS);
 }
 
