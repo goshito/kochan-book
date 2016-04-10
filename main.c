fe0789181838b9cc335c033fbf98a46e18b0289e
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-10 14:01:14 +0300

    Chapter 9, Exercise 4 - Determine week day

--- main.c
+++ main.c
@@ -8,86 +8,90 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 8, 2016, 2:13 PM
+ * Created on April 10, 2016, 1:04 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
- * Chapter 9, Exercise 3
- * solved using struct function
+ * Chapter 9, Exercise 4
  */
 
-/*
- * Write a function elapsed_time that takes as its arguments two time structures
- * and returns a time structure that represents the elapsed time (in hours,
- * minutes, and seconds) between the two times. So the call
+/* N = 1461 x f(year, month) / 4 + 153 x g(month) / 5 + day
+ * If you take the value of N as computed in exercise 2, subtract 621,049 from
+ * it, and then take the result modulo 7, you get a number from 0 to 6 that
+ * represents the day of the week (Sunday through Saturday, respectively) on
+ * whihc the particular day falls. For example, the value of N computed for
+ * August 8, 2004, is 732,239 as derived previously. 732,239 - 621,049 gives
+ * 11,190, and 111,190 % 7 gives 2, indicating that this date falls on Tuesday.
  * 
- * elapsed_time(time1, time2)
- * 
- * where time1 represents 3:45:15 and time2 represents 9:44:03, should return a
- * time structure that represents 5 hours, 58 minutes, and 48 seconds. Be 
- * careful with times that cross midnight. 
- */
+ * Use the functions developed in the previous exercise to develop a program
+ * that displays the day of the week on which a particular date falls. Make
+ * certain that the program displays the day of the week in English (such as
+ * "Monday"). */
 
-/*
- * Algorithm that I will use:
- * 1. Define/declare the struct that will hold the time
- * 2. For each 60 seconds passed a minute should be added 
- *    For each 60 minutes passed an hour should be added.
- *    I need a function to update elapsed time until it reaches time 2
- *    Maybe a loop
- * 3. Allow the user to enter time1 and time2 and store them appropriately
- * 4. Determine how to calculate the elapsed time between time1 and time2
- */
+struct date {
+        int month;
+        int day;
+        int year;
+    };
 
-struct time {
-    int hour;
-    int minutes;
-    int seconds;
-};
+// Function to calculate f(year, month) and return the value of year 
 
-int absoluteValue(int x) {
-    if (x < 0)
-        return -x;
+int f(int year, int month) {
+    if (month <= 2)
+        return year - 1;
+    else
+        return year;
 }
 
-struct time elapsed_time(struct time time1, struct time time2) {
-    struct time elapsed_time;
+// Function to calculate g(month) 
+
+int g(int month) {
+    if (month <= 2)
+        return month + 13;
+    else
+        return month + 1;
+}
+
+// Function to calculate N
+
+int calcDays(struct date date) {
+    int n;
     
-    elapsed_time.hour = time2.hour - time1.hour;
-    elapsed_time.minutes = time2.minutes - time1.minutes;
-    elapsed_time.seconds = time2.seconds - time1.seconds;
+    n = 1461 * f(date.year, date.month) / 4 + 153 * g(date.month) / 5 + date.day;
     
-    if (elapsed_time.hour < 0)
-        elapsed_time.hour = absoluteValue(elapsed_time.hour);
-    
-    if (elapsed_time.minutes < 0) {
-        elapsed_time.hour += elapsed_time.minutes;
-        elapsed_time.minutes += 60;
-    }
-    
-    if (elapsed_time.seconds < 0) {
-        elapsed_time.minutes -= 1;
-        elapsed_time.seconds += 60;
-    }
-    
-    return elapsed_time;
+    return n;
 }
 
 int main(int argc, char** argv) {
-    struct time time1, time2, t;
+    int n, week_day;
+    struct date date;
     
-    printf("Enter time1 (hh:mm:ss): \n");
-    scanf("%i:%i:%i", &time1.hour, &time1.minutes, &time1.seconds);
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf("%i/%i/%i", &date.month, &date.day, &date.year);
     
-    printf("Enter time2 (hh:mm:ss): \n");
-    scanf("%i:%i:%i", &time2.hour, &time2.minutes, &time2.seconds);
+    n = calcDays(date);
+    week_day = (n - 621049) % 7;
     
-    t = elapsed_time(time1, time2);   
-    
-    printf("%i:%i:%i\n", t.hour, t.minutes, t.seconds);
-        
+    if (week_day == 0)
+        printf("Sunday");
+    else if (week_day == 1)
+        printf("Monday");
+    else if (week_day == 2)
+        printf("Tuesday");
+    else if (week_day == 3)
+        printf("Wednesday");
+    else if (week_day == 4)
+        printf("Thrusday");
+    else if (week_day == 5)
+        printf("Friday");
+    else if (week_day == 6)
+        printf("Saturday");
+    else
+        printf("!!! Critical Error !!!\n MUHAHAHAHAHAHA");
+
     return (EXIT_SUCCESS);
 }
+
