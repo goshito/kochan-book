818ad5863d34907510179d4fba382bcc3d17d3d5
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-10 17:40:39 +0300

    Chapter 9, Exercise 6 - compound literals

--- main.c
+++ main.c
@@ -8,22 +8,20 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on April 10, 2016, 2:07 PM
+ * Created on April 10, 2016, 5:03 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-/*
- * Chapter 9, Exercise 5
+/* 
+ *  Chapter 9, Exercise 6
  */
 
-/* Write a function called clockKeeper that takes as its argument a dateAndTime
- * structure as defined in this chapter. The function should call the timeUpdate
- * function, and if the time reaches midnight, the function should call the
- * dateUpdate function to switch over to the next day. Have the function return
- * the updated dateAndTime structure. */
+/* Replace the dateUpdate function from Program 9.4 with the modified one that
+ * uses compound literals as presented in the text. Run the program to verify
+ * its proper operation.*/
 
 struct date {
     int month;
@@ -31,36 +29,37 @@ struct date {
     int year;
 };
 
-struct time {
-    int hour;
-    int minutes;
-    int seconds;
-};
+// Function to calculate tomorrow's date - using compound literals
 
-struct dateAndTime {
-    struct date sdate;
-    struct time stime;
-};
-
-// Function to update time by one second
-
-struct time timeUpdate(struct time now) {
-    ++now.seconds;
+struct date dateUpdate(struct date today) {
     
-    if (now.seconds == 60) {        // next minute
-        now.seconds = 0;
-        ++now.minutes;
-        
-        if (now.minutes == 60) {    // next hour
-            now.minutes = 0;
-            ++now.hour;
-            
-            if (now.hour == 24)     // midnight
-                now.hour = 0;
-        }
+    struct date tomorrow;
+    int numberOfDays(struct date d);
+    
+    if (today.day != numberOfDays(today)) {
+        tomorrow = (struct date) {today.month, today.day + 1, today.year};
+        /*
+        tomorrow.day = today.day + 1;
+        tomorrow.month = today.month;
+        tomorrow.year = today.year;
+        */
+    } else if (today.month == 12) {     // end of year
+        tomorrow = (struct date) {1, 1, today.year + 1};
+        /*
+        tomorrow.day = 1;
+        tomorrow.month = 1;
+        tomorrow.year = today.year + 1;
+        */
+    } else {                            // end of month
+        tomorrow = (struct date) {1, today.month + 1, today.year};
+        /*
+        tomorrow.day = 1;
+        tomorrow.month = today.month + 1;
+        tomorrow.year = today.year;
+        */
     }
     
-    return now;
+    return tomorrow;
 }
 
 // Function to find the number of days in a month
@@ -68,7 +67,7 @@ struct time timeUpdate(struct time now) {
 int numberOfDays(struct date d) {
     int days;
     bool isLeapYear(struct date d);
-    const int daysPerMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    const int daysPerMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     
     if (isLeapYear && d.month == 2)
         days = 29;
@@ -78,12 +77,12 @@ int numberOfDays(struct date d) {
     return days;
 }
 
-// Function to determine if it's a leap year
+// Function to determine of it's a leap year
 
 bool isLeapYear(struct date d) {
     bool leapYearFlag;
     
-    if ((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0)
+    if ((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0 )
         leapYearFlag = true;    // It's a leap year
     else
         leapYearFlag = false;   // Not a leap year
@@ -91,52 +90,17 @@ bool isLeapYear(struct date d) {
     return leapYearFlag;
 }
 
-// Function to calculate tomorrow's date
-
-struct date dateUpdate(struct date today) {
-    struct date tomorrow;
-    int numberOfDays(struct date d);
-    
-    if (today.day != numberOfDays(today)) {
-        tomorrow.day = today.day + 1;
-        tomorrow.month = today.month;
-        tomorrow.year = today.year;
-    } else if (today.month == 12) {     // end of year
-        tomorrow.day = 1;
-        tomorrow.month = 1;
-        tomorrow.year = today.year + 1;
-    } else {                            // end of month
-        tomorrow.day = 1;
-        tomorrow.month = today.month + 1;
-        tomorrow.year = today.year;
-    }
-    
-    return tomorrow;
-}
-
-// And here's the fuckin' clockKeeper function
-
-struct dateAndTime clockKeeper(struct dateAndTime dat) {
-    dat.stime = timeUpdate(dat.stime);
-    if (dat.stime.hour == 0)
-        dat.sdate = dateUpdate(dat.sdate);
-    
-    return dat;
-}
-
 int main(int argc, char** argv) {
-    struct dateAndTime test;
+    struct date dateUpdate(struct date today);
+    struct date thisDay, nextDay;
     
-    printf("Enter date (mm/dd/yyyy): ");
-    scanf("%i/%i/%i", &test.sdate.month, &test.sdate.day, &test.sdate.year);
-    printf("\nEnter time (hh:mm:ss): ");
-    scanf("%i:%i:%i", &test.stime.hour, &test.stime.minutes, &test.stime.seconds);
+    printf("Enter today's date (mm dd yyyy): ");
+    scanf("%i%i%i", &thisDay.month, &thisDay.day, &thisDay.year);
     
-    test = clockKeeper(test);
+    nextDay = dateUpdate(thisDay);
     
-    printf("\nDate:\n%i/%i/%i", test.sdate.month, test.sdate.day, test.sdate.year);
-    printf("\nTime:\n%i:%i:%i", test.stime.hour, test.stime.minutes, test.stime.seconds);
-    
+    printf("Tomorrow's date is %i/%i/%.2i.\n", nextDay.month, nextDay.day, nextDay.year % 100);
+
     return (EXIT_SUCCESS);
 }
 
