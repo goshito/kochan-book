302bae4ccb9cac3ba6b33b03d2fbf35549168876
georgi <georgi@georgi-Aspire-E5-572G>
2016-04-15 21:16:00 +0300

    Chapter 10, Exercise 7, hourly release :) (with function) bot not quite working..

--- main.c
+++ main.c
@@ -32,56 +32,34 @@
  * returned.
  */
 
-void insertString(char source_str[], char str_to_insert[], int pos) {
+void insertString(char source[], char insert[], int pos) {
+    int sourceSize = strlen(source);
+    int insertSize = strlen(insert);
+    int strSumSizes = strlen(source) + strlen(insert);
     
-    // go to i-th position
-    int i = 0;
-    while (i <= pos) {
-        i++;
-    }
+    source[pos + insertSize] = source[pos];
+    //printf("%c", source[pos + insertSize]);
+    pos++;
+    source[pos + insertSize] = source[pos];
+    //printf("%c", source[pos + insertSize]);
+    pos++;
+    source[pos + insertSize] = source[pos];
+    //printf("%c", source[pos + insertSize]);
+    source[pos + insertSize + 1] = '\0';
     
-    while (i < strlen(source_str)) {
-        source_str[i] = ' ';
-    }
-    printf("%s", source_str);
+    pos = (sourceSize - insertSize);
+    source[pos] = insert[0];
+    pos++;
+    source[pos] = insert[1];
+    pos++;
+    source[pos] = insert[2];
 }
 
 int main(int argc, char** argv) {
     char text[] = "the wrong son";
     
     insertString(text, "per", 10);
+    printf("%s", text);
 
     return (EXIT_SUCCESS);
 }
-
-/*#include <stdio.h>
-#include <string.h>
-
-int main(void) {
-    char src_str[] = "the wrong son";
-    char str_ins[] = "per";
-    int i, ins_pos, new_str_size; 
-    
-    ins_pos = (strlen(src_str) - (strlen(str_ins)));
-    new_str_size = strlen(src_str) + strlen(str_ins);
-    
-    // free up space for insertion
-    i = ins_pos + strlen(str_ins);
-    while (i <= new_str_size) {
-        src_str[i] = src_str[i];
-        i--;
-    }
-    
-    
-    while (ins_pos < strlen(src_str)) {        
-        src_str[ins_pos] = str_ins[i];
-        ins_pos += sizeof(str_ins);
-        ins_pos++;
-        //i++;
-    }
-    
-    printf("src_str = %s", src_str);
-    
-    return 0;
-}
-*/
