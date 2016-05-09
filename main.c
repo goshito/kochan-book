c6b54c1301cb913574cc3e095e3fdf4b7ddbc8e7
georgi <georgi@georgi-Aspire-E5-572G>
2016-05-09 14:48:53 +0300

    * Chapter 10, Exercise 12  *  * Write a function called strToFloat that converts a character string into a  * floating point value. Have the function accept an optional leading minus.  * So, the call  *  * strToFloar("-867.6921");  *  * should return the value -867.6921

--- main.c
+++ main.c
@@ -8,45 +8,71 @@
  * File:   main.c
  * Author: georgi
  *
- * Created on May 3, 2016, 7:04 PM
+ * Created on May 4, 2016, 1:59 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 /*
- * Chapter 10, Exercise 11
+ * Chapter 10, Exercise 12
  * 
- * Extend the strToInt function from Program 10.11 so that if the first charac-
- * ter of the string is a minus sign, the value that follows is taken as a nega-
- * tive number.
+ * Write a function called strToFloat that converts a character string into a
+ * floating point value. Have the function accept an optional leading minus.
+ * So, the call
+ * 
+ * strToFloar("-867.6921");
+ * 
+ * should return the value -867.6921
  */
 
-int strToInt(const char string[]) { 
-    int i, intValue, result = 0;
+// flag the start of the decimal point !!!
+
+double strToFloat(char string[]) { // type cast???
+    int i, decimalCount = 0;
+    double floatValue, result = 0;
+    bool decimalPoint = false;
     
+    // handle minus
     if (string[0] == '-') {
-       for (i = 1; string[i] >= '0' && string[i] <= '9'; ++i) {
-           intValue = string[i] - '0';
-           result = result * 10 + intValue;  
+       for (i = 1; string[i] >= '.' && string[i] <= '9'; ++i) {
+           if (string[i] == '/') { //skip the backslash
+                i++;
+                continue;
+            } else if (string[i] == '.') { //skip the decimal point
+                continue;
+            }    
+           
+           floatValue = string[i] - '0';
+           result = result * 10 + floatValue;  
        }
-       return -result;
-    } else {
-        for (i = 0; string[i] >= '0' && string[i] <= '9'; ++i) {
-           intValue = string[i] - '0';
-           result = result * 10 + intValue; 
+       return -result * 0.0001;
+    } else {                  // calculate positive floating point number      
+        for (i = 0; string[i] >= '.' && string[i] <= '9'; ++i) {
+            
+            if (string[i] == '/') { //skip the backslash
+                i++;
+                continue;
+            } else if (string[i] == '.') { //skip the decimal period
+                decimalPoint = true; // decimal point start
+                continue;
+            }             
+            while (decimalCount < i + decimalCount)
+                decimalCount++;
+            // count the number of digits after a floating point
+            floatValue = string[i] - '0';
+            result = result * 10 + floatValue; 
         }
-       return result; 
+       return result * 0.0001; 
     }    
     
 }
 
 int main(int argc, char** argv) {
-    int strToInt(const char string[]);
-    
-    printf("%i\n", strToInt("-245"));
-    printf("%i\n", strToInt("100") + 25);
-    printf("%i\n", strToInt("13x5"));
+    printf("%f", strToFloat("-867.6921"));
 
     return (EXIT_SUCCESS);
 }
+
