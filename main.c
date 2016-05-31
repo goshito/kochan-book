46e257449795efb21711955875481e29956eda4d
georgi <georgi@georgi-Aspire-E5-572G>
2016-05-31 12:45:12 +0300

    added comments and changed variables names of the insert function to be more readale

--- main.c
+++ main.c
@@ -5,63 +5,72 @@
  */
 
 /* 
- * File:   main.c
+ * File:   insEntry.c
  * Author: georgi
  *
- * Created on May 13, 2016, 11:30 AM
+ * Created on May 29, 2016, 2:21 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/* Chapter 10, Exercise 14
+/*Chapter 11, Exercise 2
  * 
- * Write a function called intToStr that converts an integer value into a 
- * character string. Be certain the function handles negative integers properly.
+ * Write a function called insertEntry to insert a new entry into a linked list. Have
+the procedure take as arguments a pointer to the list entry to be inserted (of type
+struct entry as defined in this chapter), and a pointer to an element in the list
+after which the new entry is to be inserted.
  */
 
-void intToStr(int value, char str[]) { 
-    int value_tmp, digit_count, digit, i, origin_value;
-    //backup original value for later use
-    origin_value = value;
-    // treat value as positive for the convert, will add minus to str[0] later
-    if (value < 0) {
-        value = -value;
-    }
-    
-    // make a copy of value because I need to use original value later
-    value_tmp = value;
-    
-    // count the number of digits
-    for (digit_count = 0; value_tmp >= 1; digit_count++) {
-        value_tmp /= 10;
-    }
-    
-    //extract the digits and handle minus situation
-    if (origin_value < 0) {
-        i = digit_count;
-        str[0] = '-';
-    }
-    else
-        i = digit_count - 1;
-    do {
-        digit = value % 10;
-        str[i] = digit + '0';
-        value = value / 10;
-        i--;
-    } while (value >= 1);
+struct entry {
+    int value;
+    struct entry *next;
+};
+//function to insert an entry into the linked list
+void insertEntry(struct entry *insert, struct entry *after) {
+    insert->value = 250;
+    insert->next = after->next;
+    after->next = insert;
 }
-int main(int argc, char** argv) {
-    void intToStr(int value, char str[]);
-    char numStr[100];
+//function to display the contents of the linked list
+void printList(struct entry *list_pointer) {
     
-    intToStr(-1234, numStr);
+    while (list_pointer != (struct entry *) 0) {
+        printf("%i", list_pointer->value);
+        list_pointer = list_pointer->next;
+        if (list_pointer)
+            printf(",");
+    }
     
-    printf("%s\n", numStr);
+}
 
+int main(int argc, char** argv) {
+    
+    struct entry n1, n2, n3, *list_start = &n1;
+    struct entry n21; // this will be inserted
+    struct entry *ptr_n2 = &n2, *ptr_n21 = &n21;    
+    
+    n1.value = 100;
+    n1.next = &n2;
+    
+    n2.value = 200;
+    n2.next = &n3;
+    
+    n3.value = 300;
+    n3.next = (struct entry *) 0;
+    
+    //display the linked list before insert
+    printList(list_start);
+    //insert n21 after n2
+    insertEntry(ptr_n21, ptr_n2);
+    //display the linked list
+    printList(list_start);
+    
     return (EXIT_SUCCESS);
 }
 
-// backup from before adding else condition
-
+/* 
+ * 1. create linked list with ?sample elements 
+ * 2. Program works, now set the variables readable names
+ *       
+ */
