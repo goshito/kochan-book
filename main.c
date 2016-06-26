693f57e93804fc66a84c4c9f8ebd26f9a65f1328
georgi <georgi@georgi-Aspire-E5-572G>
2016-06-27 02:04:53 +0300

    Chapter 11, Exercise 3, solved with help from a fellow programmer

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Chapter 11, Exercise 2
+/*Chapter 11, Exercise 3
  * 
  * Write a function called insertEntry to insert a new entry into a linked list. Have
 the procedure take as arguments a pointer to the list entry to be inserted (of type
@@ -26,29 +26,14 @@ struct entry {
     int value;
     struct entry *next;
 };
-//function to insert an entry into the linked list
-void insertEntry(struct entry *insert, struct entry *after) {
-    insert->value = 250;
-    insert->next = after->next;
-    after->next = insert;
-}
-//function to display the contents of the linked list
-void printList(struct entry *list_pointer) {
-    
-    while (list_pointer != (struct entry *) 0) {
-        printf("%i", list_pointer->value);
-        list_pointer = list_pointer->next;
-        if (list_pointer)
-            printf(",");
-    }
-    
-}
 
 int main(int argc, char** argv) {
+    void print_list(struct entry *list_pointer);
+    void insert_entry(struct entry *insert, struct entry *after);    
     
-    struct entry n1, n2, n3, *list_start = &n1;
-    struct entry n21; // this will be inserted
-    struct entry *ptr_n2 = &n2, *ptr_n21 = &n21;    
+    struct entry n0, n1, n2, n3, start;
+    
+    start.next = &n1;
     
     n1.value = 100;
     n1.next = &n2;
@@ -60,17 +45,45 @@ int main(int argc, char** argv) {
     n3.next = (struct entry *) 0;
     
     //display the linked list before insert
-    printList(list_start);
-    //insert n21 after n2
-    insertEntry(ptr_n21, ptr_n2);
-    //display the linked list
-    printList(list_start);
+    print_list(start.next);
+    
+    //insert n0
+    n0.value = 50;
+    //
+    insert_entry(&n0, &start);
+    //display the linked list after insertion of an element at the beginning
+    print_list(start.next);
     
     return (EXIT_SUCCESS);
 }
 
+//
+void insert_entry(struct entry *insert, struct entry *after) {
+    
+    insert->next = after->next;
+    after->next = insert;
+    
+}
+
+// function to display the contents of a linked list
+void print_list(struct entry *list_pointer) {
+    
+    while (list_pointer != (struct entry *) 0) {
+        printf("%i, ", list_pointer->value);
+        list_pointer = list_pointer->next;
+    }
+    printf("\n");
+    
+}
+
+
+
 /* 
  * 1. create linked list with ?sample elements 
  * 2. Program works, now set the variables readable names
+ * 
+ * struct entry *ptr_n2 = &n2, *ptr_n21 = &n21; and *list_start..
+ * are these necessary?
+ * 3. I will remove list_start pointer
  *       
  */
